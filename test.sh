f140894d6c65da5e84177712047ad1af224d6655
David Shen <me@pantherman594.com>
2018-01-12 00:19:32 -0500

    Update script

--- test.sh
+++ test.sh
@@ -4,14 +4,15 @@ dir=$PWD
 
 cd ~ubuntu/bc18-scaffold
 
-#git reset --hard
-#git clean -fdx
-#git pull
+git reset --hard
+git clean -fdx
+git pull
 
 mkdir -p Bot
 mkdir -p replays
 
 cp -r $dir/Bot/src/* ./Bot/
+cp examplefuncsplayer-java/run.sh ./Bot/
 
 sed -i '2 i\python3() {\n    ~ubuntu/.pyenv/versions/general/bin/python $@\n}\npip3() {\n    ~ubuntu/.pyenv/versions/general/bin/pip $@\n}' battlecode.sh
 bash ./battlecode.sh -p1 Bot -p2 examplefuncsplayer-python -m testmap
