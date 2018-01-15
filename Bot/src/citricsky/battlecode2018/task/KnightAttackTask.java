36da7721b0ca62084eb0e900d86212ef46dbe01a
SkyStruck <kevinqi6@gmail.com>
2018-01-14 22:21:53 -0500

    Added KnightAttackTask

---
+++ Bot/src/citricsky/battlecode2018/task/KnightAttackTask.java
@@ -0,0 +1,45 @@
+package citricsky.battlecode2018.task;
+
+import java.util.function.Predicate;
+
+import citricsky.battlecode2018.library.Direction;
+import citricsky.battlecode2018.library.GameController;
+import citricsky.battlecode2018.library.MapLocation;
+import citricsky.battlecode2018.library.Unit;
+import citricsky.battlecode2018.unithandler.PathfinderTask;
+
+public class KnightAttackTask implements PathfinderTask{
+
+	private static final Predicate<MapLocation> STOP_CONDITION = location -> {
+		return getEnemyDirection(location) != null;
+	};
+	private static Direction getEnemyDirection(MapLocation location) {
+		for(Direction direction: Direction.CARDINAL_DIRECTIONS) {
+			MapLocation offset = location.getOffsetLocation(direction);
+			if(GameController.INSTANCE.canSenseLocation(offset)) {
+				if(offset.hasUnitAtLocation()) {
+					if(offset.getUnit().getTeam().equals(GameController.INSTANCE.getEnemyTeam())) {
+						return direction;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public void execute(Unit unit, MapLocation location) {
+		if(unit.getLocation().getMapLocation().equals(location)) {
+			Direction direction = getEnemyDirection(location);
+			if(unit.canAttack(location.getOffsetLocation(direction).getUnit())) {
+				if(unit.isAttackReady()) {
+					unit.attack(location.getOffsetLocation(direction).getUnit());
+				}
+			}
+		}
+	}
+	@Override
+	public Predicate<MapLocation> getStopCondition() {
+		return STOP_CONDITION;
+	}
+}
