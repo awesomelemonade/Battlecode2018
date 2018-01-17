f21da5c05b1c3c992993025cbeb60534776a5275
awesomelemonade <awesomelemonade@gmail.com>
2018-01-17 18:51:36 -0500

    Updated MarsPlayer

--- Bot/src/citricsky/battlecode2018/main/MarsPlayer.java
+++ Bot/src/citricsky/battlecode2018/main/MarsPlayer.java
@@ -6,33 +6,33 @@ import citricsky.battlecode2018.library.Unit;
 import citricsky.battlecode2018.library.UnitType;
 import citricsky.battlecode2018.task.*;
 import citricsky.battlecode2018.unithandler.*;
+import citricsky.battlecode2018.util.Benchmark;
 import citricsky.battlecode2018.util.Util;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 
 public class MarsPlayer {
 	public static void execute() {
+		Benchmark benchmark = new Benchmark();
 		GameController gc = GameController.INSTANCE;
 		Map<UnitType, Set<PathfinderTask>> pathfinderTasks = new HashMap<UnitType, Set<PathfinderTask>>();
 		Map<UnitType, Set<Function<Unit, UnitHandler>>> handlers = new HashMap<UnitType, Set<Function<Unit, UnitHandler>>>();
 		for (UnitType unitType : UnitType.values()) {
 			handlers.put(unitType, new HashSet<Function<Unit, UnitHandler>>());
-			pathfinderTasks.put(unitType, new HashSet<PathfinderTask>());
+			pathfinderTasks.put(unitType, new LinkedHashSet<PathfinderTask>());
 		}
-		pathfinderTasks.get(UnitType.WORKER).add(new WorkerHarvestTask());
-		pathfinderTasks.get(UnitType.WORKER).add(new WorkerBuildTask());
 		pathfinderTasks.get(UnitType.WORKER).add(new WorkerReplicateTask());
+		pathfinderTasks.get(UnitType.WORKER).add(new WorkerHarvestTask());
 		pathfinderTasks.get(UnitType.KNIGHT).add(new KnightAttackTask());
 		pathfinderTasks.get(UnitType.RANGER).add(new RangerAttackTask());
 		pathfinderTasks.get(UnitType.MAGE).add(new MageAttackTask());
 		pathfinderTasks.get(UnitType.HEALER).add(new HealerHealTask());
 
-		handlers.get(UnitType.FACTORY).add(FactoryHandler::new);
-		handlers.get(UnitType.ROCKET).add(RocketHandler::new);
 		handlers.get(UnitType.KNIGHT).add(ExploreHandler::new);
 		handlers.get(UnitType.RANGER).add(ExploreHandler::new);
 		handlers.get(UnitType.MAGE).add(ExploreHandler::new);
@@ -62,17 +62,20 @@ public class MarsPlayer {
 				}
 			}
 		}
-		int lastRoundNumber = GameController.INSTANCE.getRoundNumber();
 		while (true) {
+			benchmark.push();
 			RoundInfo.update();
-			if(RoundInfo.getRoundNumber() > lastRoundNumber + 1) {
-				System.out.println("Skipped Round? "+lastRoundNumber+" - "+RoundInfo.getRoundNumber());
-			}
-			lastRoundNumber = RoundInfo.getRoundNumber();
 			//System.out.println("Round: " + GameController.INSTANCE.getRoundNumber() + " Time: " + GameController.INSTANCE.getTimeLeft() + "ms Karbonite: " + GameController.INSTANCE.getCurrentKarbonite());
 			occupied.clear();
 			for(UnitType unitType : UnitType.values()) {
-				pathfinderTasks.get(unitType).forEach(PathfinderTask::update);
+				for (PathfinderTask task : pathfinderTasks.get(unitType)) {
+					benchmark.push();
+					task.update();
+					double deltaTime = benchmark.pop() / 1000000.0;
+					if (deltaTime > 10) {
+						System.out.println("Update: " + task.getClass().getSimpleName() + " - " + deltaTime + "ms");
+					}
+				}
 			}
 			Unit[] myUnits = gc.getMyUnits();
 			Map<Unit, Set<UnitHandler>> map = new HashMap<Unit, Set<UnitHandler>>();
@@ -82,7 +85,7 @@ public class MarsPlayer {
 					map.get(unit).add(function.apply(unit));
 				}
 			}
-			while (gc.getTimeLeft() > 1000) {
+			while (benchmark.peek() / 1000000 < gc.getTimeLeft() - 1000) {
 				Unit bestUnit = null;
 				UnitHandler bestHandler = null;
 				int bestPriority = Integer.MIN_VALUE;
@@ -115,6 +118,10 @@ public class MarsPlayer {
 					System.out.println(ex.getMessage());
 					ex.printStackTrace();
 				}
+			}
+			double deltaTime = benchmark.pop() / 1000000.0;
+			if(deltaTime > 20) {
+				System.out.println("Round: " + RoundInfo.getRoundNumber() + " - " + deltaTime + "/" + gc.getTimeLeft() + "ms");
 			}
 			gc.yield();
 		}
