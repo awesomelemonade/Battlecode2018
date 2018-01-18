a8ddf9b197eac798be3ad1d230abb788fa73fc8d
awesomelemonade <awesomelemonade@gmail.com>
2018-01-17 19:12:51 -0500

    Changed Research Order

--- Bot/src/citricsky/battlecode2018/main/EarthPlayer.java
+++ Bot/src/citricsky/battlecode2018/main/EarthPlayer.java
@@ -22,13 +22,13 @@ public class EarthPlayer {
 		GameController gc = GameController.INSTANCE;
 		UnitType[] researchOrder = new UnitType[]
 				{UnitType.WORKER, //finishes at Round 25
-				 UnitType.RANGER, //finishes at Round 50
-				 UnitType.KNIGHT, //finishes at Round 75
-				 UnitType.WORKER, //finishes at Round 150
-				 UnitType.RANGER, //finishes at Round 250
-				 UnitType.KNIGHT, //finishes at Round 325
+				 UnitType.ROCKET, //finishes at Round 125
+				 UnitType.RANGER, //finishes at Round 150
+				 UnitType.KNIGHT, //finishes at Round 175
+				 UnitType.WORKER, //finishes at Round 250
+				 UnitType.RANGER, //finishes at Round 350
+				 UnitType.KNIGHT, //finishes at Round 425
 				 UnitType.KNIGHT, //finishes at Round 475
-				 UnitType.ROCKET, //finishes at Round 575
 				 UnitType.WORKER, //finishes at Round 650
 				 UnitType.ROCKET, //finishes at Round 725
 				 UnitType.HEALER, //finishes at Round 750
