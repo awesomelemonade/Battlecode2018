11e01a7e88aab46e030632fdb356dde2954c23c9
awesomelemonade <awesomelemonade@gmail.com>
2018-01-24 12:39:59 -0500

    Simplification of BFS in BFSTest

--- BFSTest/src/BFS.java
+++ BFSTest/src/BFS.java
@@ -4,16 +4,16 @@ import java.util.function.Predicate;
 
 public class BFS {
 	private Predicate<Vector> passable;
-	//Least significant bit = Stores whether it has been visited
-	//2nd-9th least significant bits = stores direction from source
-	//10th-17th least significant bits = stores direction to source
-	//18th-32nd bits = max = 32768 = step number
-	private static final int FROM_SHIFT = 0;
-	private static final int TO_SHIFT = 8;
+	
+	private static final int DIRECTION_SHIFT = 0;
+	private static final int DIRECTION_BITMASK = 0b1111;
+	private static final int SOURCE_SHIFT = 4;
+	private static final int SOURCE_BITMASK = 0b111111111111; //12 bits = 4096, max = 50 * 50 = 2500
 	private static final int STEP_SHIFT = 16;
-	private static final int DIRECTION_BITMASK = 0b11111111;
-	private static final int STEP_BITMASK = 0b1111111111111111;
+	private static final int STEP_BITMASK = 0b111111111111; //12 bits = 4096, max = 50 * 50 = 2500
+	
 	public static final int SOURCE_STEP = 0b1;
+	
 	private int[][] data;
 	
 	private Deque<Vector> queue;
@@ -24,17 +24,20 @@ public class BFS {
 		this.passable = passable;
 		this.queue = new ArrayDeque<Vector>();
 		this.step = SOURCE_STEP + 1;
-		for(Vector source: sources) {
-			addSource(source);
+		for (int i = 0; i < sources.length; ++i) {
+			addSource(sources[i], i);
 		}
 	}
-	public void addSource(Vector source) {
+	public void addSource(Vector source, int sourceId) {
 		if(outOfBounds(source)) {
 			return;
 		}
 		if((data[source.getX()][source.getY()] >>> STEP_SHIFT) != SOURCE_STEP) {
 			queue.add(source);
-			data[source.getX()][source.getY()] = (SOURCE_STEP << STEP_SHIFT);
+			data[source.getX()][source.getY()] =
+					((Direction.CENTER.ordinal() & DIRECTION_BITMASK) << DIRECTION_SHIFT) |
+					((sourceId & SOURCE_BITMASK) << SOURCE_SHIFT) |
+					((SOURCE_STEP & STEP_BITMASK) << STEP_SHIFT);
 		}
 	}
 	public void reset() {
@@ -65,24 +68,24 @@ public class BFS {
 	public int getHeight() {
 		return data[0].length;
 	}
-	public int getDirectionFromSource(int x, int y) {
-		if(outOfBounds(x, y)) {
-			return 0;
+	public int getSourceId(int x, int y) {
+		if (getStep(x, y) == Integer.MAX_VALUE) {
+			return -1;
 		}
-		return (data[x][y] >>> FROM_SHIFT) & DIRECTION_BITMASK;
+		return (data[x][y] >>> SOURCE_SHIFT) & SOURCE_BITMASK;
 	}
-	public int getDirectionToSource(int x, int y) {
-		if(outOfBounds(x, y)) {
-			return 0;
+	public Direction getDirectionToSource(int x, int y) {
+		if (getStep(x, y) == Integer.MAX_VALUE) {
+			return null;
 		}
-		return (data[x][y] >>> TO_SHIFT) & DIRECTION_BITMASK;
+		return Direction.values()[(data[x][y] >>> DIRECTION_SHIFT) & DIRECTION_BITMASK];
 	}
 	public int getStep(int x, int y) {
-		if(outOfBounds(x, y)) {
+		if (outOfBounds(x, y)) {
 			return Integer.MAX_VALUE;
 		}
 		int step = (data[x][y] >>> STEP_SHIFT) & STEP_BITMASK;
-		if(step == 0) {
+		if (step == 0) {
 			return Integer.MAX_VALUE;
 		} else {
 			return step;
@@ -97,21 +100,16 @@ public class BFS {
 	public void step() {
 		for(int i = 0, size = queue.size(); i < size; ++i) {
 			Vector vector = queue.poll();
+			int sourceId = (data[vector.getX()][vector.getY()] >>> SOURCE_SHIFT) & SOURCE_BITMASK;
 			for(Direction direction: Direction.COMPASS) {
 				Vector candidate = vector.add(direction.getOffsetVector());
 				if((!outOfBounds(candidate)) && passable.test(candidate)) {
-					int currentStep = (data[candidate.getX()][candidate.getY()] >>> STEP_SHIFT) & STEP_BITMASK;
-					if(currentStep == 0) { //check whether step has been set
-						data[candidate.getX()][candidate.getY()] |= (step << STEP_SHIFT);
+					if (data[candidate.getX()][candidate.getY()] == 0) {
+						data[candidate.getX()][candidate.getY()] =
+								((direction.getOpposite().ordinal() & DIRECTION_BITMASK) << DIRECTION_SHIFT) |
+								((sourceId & SOURCE_BITMASK) << SOURCE_SHIFT) |
+								((step & STEP_BITMASK) << STEP_SHIFT);
 						queue.add(candidate);
-					}
-					if(currentStep == 0 || currentStep == step) {
-						if(((data[vector.getX()][vector.getY()] >>> STEP_SHIFT) & STEP_BITMASK) == SOURCE_STEP) { //checks whether vector is source
-							data[candidate.getX()][candidate.getY()] |= (1 << (direction.ordinal() + FROM_SHIFT)); 
-						}else {
-							data[candidate.getX()][candidate.getY()] |= (data[vector.getX()][vector.getY()] & (DIRECTION_BITMASK << FROM_SHIFT));
-						}
-						data[candidate.getX()][candidate.getY()] |= (1 << (direction.getOpposite().ordinal() + TO_SHIFT)); //direction to source
 					}
 				}
 			}
--- BFSTest/src/BFSMain.java
+++ BFSTest/src/BFSMain.java
@@ -1,13 +1,6 @@
 
 public class BFSMain {
 	public static void main(String[] args) {
-		for(Direction direction: Direction.values()) {
-			if(direction == Direction.CENTER) {
-				continue;
-			}
-			int encoded = 1 << direction.ordinal();
-			System.out.println(direction+"="+getFormatted(encoded));
-		}
 		int[][] map= new int[][] {
 				{0, 0, 0, 0, 0, 0, 0, 0},
 				{0, 0, 1, 0, 0, 0, 0, 0},
@@ -18,38 +11,42 @@ public class BFSMain {
 				{0, 0, 1, 0, 1, 1, 0, 0},
 				{0, 0, 1, 0, 0, 0, 0, 0}
 		};
-		BFS bfs = new BFS(map.length, map[0].length, vector -> map[vector.getX()][vector.getY()]==0, new Vector(1, 1));
+		BFS bfs = new BFS(map.length, map[0].length, vector -> map[vector.getX()][vector.getY()]==0);
+		bfs.addSource(new Vector(1, 1), 0);
 		while(bfs.getQueue().size()>0) {
 			bfs.step();
 		}
 		printBFS(bfs);
 	}
 	public static void printBFS(BFS bfs) {
-		System.out.println("BFS[DirectionFromSource]");
+		System.out.println("BFS[SourceId]");
 		for(int j=0;j<bfs.getHeight();++j) {
+			System.out.print('|');
 			for(int i=0;i<bfs.getWidth();++i) {
-				System.out.print(getFormatted(bfs.getDirectionFromSource(i, bfs.getHeight()-j-1))+" ");
+				System.out.print(pad("        ", Integer.toString(bfs.getSourceId(i, bfs.getHeight()-j-1)))+"|");
 			}
 			System.out.println();
 		}
 		System.out.println("BFS[DirectionToSource]");
 		for(int j=0;j<bfs.getHeight();++j) {
+			System.out.print('|');
 			for(int i=0;i<bfs.getWidth();++i) {
-				System.out.print(getFormatted(bfs.getDirectionToSource(i, bfs.getHeight()-j-1))+" ");
+				Direction direction = bfs.getDirectionToSource(i, bfs.getHeight()-j-1);
+				System.out.print(pad("            ", direction == null ? "null" : direction.toString())+"|");
 			}
 			System.out.println();
 		}
 		System.out.println("BFS[Step]");
 		for(int j=0;j<bfs.getHeight();++j) {
+			System.out.print('|');
 			for(int i=0;i<bfs.getWidth();++i) {
-				System.out.print(getFormatted(bfs.getStep(i, bfs.getHeight()-j-1))+" ");
+				System.out.print(pad("        ", Integer.toString(bfs.getStep(i, bfs.getHeight()-j-1)))+"|");
 			}
 			System.out.println();
 		}
 	}
-	public static String getFormatted(int encoded) {
-		String binaryString = Integer.toBinaryString(encoded);
-		return ("00000000"+binaryString).substring(binaryString.length());
+	public static String pad(String pad, String string) {
+		return (pad.concat(string)).substring(string.length());
 	}
 	public static String getDirectionShorthand(Direction direction) {
 		switch(direction) {
