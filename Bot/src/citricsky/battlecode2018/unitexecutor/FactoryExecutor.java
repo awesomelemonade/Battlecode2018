90beaf434dbf08f2451fcce3e254915d955b62f4
awesomelemonade <awesomelemonade@gmail.com>
2018-01-25 15:28:46 -0500

    Readded Knights

--- Bot/src/citricsky/battlecode2018/unitexecutor/FactoryExecutor.java
+++ Bot/src/citricsky/battlecode2018/unitexecutor/FactoryExecutor.java
@@ -1,6 +1,8 @@
 package citricsky.battlecode2018.unitexecutor;
 
 import citricsky.battlecode2018.library.Direction;
+import citricsky.battlecode2018.library.GameController;
+import citricsky.battlecode2018.library.MapLocation;
 import citricsky.battlecode2018.library.Unit;
 import citricsky.battlecode2018.library.UnitType;
 import citricsky.battlecode2018.library.Vector;
@@ -14,11 +16,35 @@ public class FactoryExecutor implements UnitExecutor {
 		this.moveManager = moveManager;
 	}
 	
-	public UnitType getProduceType() {
+	public boolean nearEnemy(MapLocation location, int moveDistance) {
+		for (int x = -moveDistance; x <= moveDistance; ++x) {
+			for (int y = -moveDistance; y <= moveDistance; ++y) {
+				Vector candidate = location.getPosition().add(x, y);
+				if (!outOfBounds(candidate.getX(), candidate.getY(), location.getPlanet().getWidth(), location.getPlanet().getHeight())) {
+					MapLocation offset = location.getPlanet().getMapLocation(candidate);
+					if (offset.hasUnitAtLocation()) {
+						if (offset.getUnit().getTeam() == GameController.INSTANCE.getEnemyTeam()) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean outOfBounds(int x, int y, int width, int height) {
+		return x < 0 || y < 0 || x >= width || y >= height;
+	}
+	
+	public UnitType getProduceType(MapLocation location) {
 		if (RoundInfo.getRoundNumber() > 650) {
 			if (RoundInfo.getUnitCount(UnitType.WORKER) < 10) {
 				return UnitType.WORKER;
 			}
+		}
+		if (nearEnemy(location, 10)) {
+			return UnitType.KNIGHT;
 		}
 		if (RoundInfo.getUnitCount(UnitType.WORKER) * 2 - 6 < RoundInfo.getUnitCount(UnitType.FACTORY)) {
 			return UnitType.WORKER;
@@ -31,7 +57,7 @@ public class FactoryExecutor implements UnitExecutor {
 	
 	@Override
 	public void execute(Unit unit) {
-		UnitType produceType = getProduceType();
+		UnitType produceType = getProduceType(unit.getLocation().getMapLocation());
 		if (produceType != null && (produceType == UnitType.WORKER || RoundInfo.getCombatUnitsCount() < 50) && unit.canProduceRobot(produceType)) {
 			unit.produceRobot(produceType);
 		}
