1fb6b13aa46ab6f95db340495e4af01a8827fbc3
David Shen <me@pantherman594.com>
2018-01-28 01:36:22 -0500

    Updated script variable name
    Really just testing if it works

--- runner.sh
+++ runner.sh
@@ -4,7 +4,7 @@ SCRIPT=$(realpath ${0})
 PRE="00000000"
 SCAFF_DIR=~/bc18-scaffold
 BOT_DIR=~/citricsky-battlecode2018
-BOT_COM=""
+PREV_BOT_COMMIT=""
 NUMGAMES=0
 NUMWINS=0
 
@@ -76,7 +76,7 @@ runGame() {
 
     scp "${GAME_ID}.bc18z" "ubuntu@ssh.pantherman594.com:/var/www/pantherman594/battlecode/games/"
     scp "${LOGFILE}" "ubuntu@ssh.pantherman594.com:/var/www/pantherman594/battlecode/games/"
-    ssh ubuntu@ssh.pantherman594.com "sed -i '1 i${BOT_COM} ${GAME_ID} ${OPPONENT} ${MAP} ${RESULT}' /var/www/pantherman594/battlecode/games/gamedata"
+    ssh ubuntu@ssh.pantherman594.com "sed -i '1 i${PREV_BOT_COMMIT} ${GAME_ID} ${OPPONENT} ${MAP} ${RESULT}' /var/www/pantherman594/battlecode/games/gamedata"
     echo ">>>>"
 
     echo "<<<< Cleaning up"
@@ -90,7 +90,7 @@ runGame() {
 resetScaffold
 
 resetBot
-BOT_COM=$(getBotCommit)
+PREV_BOT_COMMIT=$(getBotCommit)
 copyBot
 
 ssh ubuntu@ssh.pantherman594.com "cd /var/www/pantherman594/tinyview; git pull"
@@ -98,7 +98,7 @@ ssh ubuntu@ssh.pantherman594.com "cd /var/www/pantherman594/tinyview; git pull"
 while true; do
     cd ${SCAFF_DIR}
     for map in $(ls battlecode-maps | sort -R); do
-        if [[ $(getBotCommit) != ${BOT_COM} ]]; then
+        if [[ $(getBotCommit) != ${PREV_BOT_COMMIT} ]]; then
             echo "<<<< Bot updated. Restarting script... >>>>"
             chmod +x "${SCRIPT}"
             exec "${SCRIPT}"
