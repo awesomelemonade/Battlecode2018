61a7aed62db8ac365f48ffe225aef2c193b95570
awesomelemonade <awesomelemonade@gmail.com>
2018-01-27 19:29:38 -0500

    Updated MarsPlayer

--- Bot/src/citricsky/battlecode2018/main/MarsPlayer.java
+++ Bot/src/citricsky/battlecode2018/main/MarsPlayer.java
@@ -2,7 +2,7 @@ package citricsky.battlecode2018.main;
 
 import citricsky.battlecode2018.library.GameController;
 import citricsky.battlecode2018.library.UnitType;
-import citricsky.battlecode2018.library.Vector;
+import citricsky.battlecode2018.unitexecutor.FactoryExecutor;
 import citricsky.battlecode2018.unitexecutor.HealerExecutor;
 import citricsky.battlecode2018.unitexecutor.KnightExecutor;
 import citricsky.battlecode2018.unitexecutor.MageExecutor;
@@ -12,8 +12,6 @@ import citricsky.battlecode2018.unitexecutor.UnitExecutor;
 import citricsky.battlecode2018.unitexecutor.WorkerExecutor;
 import citricsky.battlecode2018.util.Benchmark;
 
-import java.util.Map;
-
 public class MarsPlayer {
 	public static void execute() {
 		Benchmark benchmark = new Benchmark();
@@ -21,34 +19,42 @@ public class MarsPlayer {
 		UnitExecutor[] executors = new UnitExecutor[UnitType.values().length];
 		MoveManager moveManager = new MoveManager();
 		PlanetCommunication communication = new PlanetCommunication();
-
-		executors[UnitType.FACTORY.ordinal()] = null;
+		
+		executors[UnitType.FACTORY.ordinal()] = new FactoryExecutor(moveManager);
 		executors[UnitType.ROCKET.ordinal()] = new RocketExecutor(moveManager, communication);
 		executors[UnitType.RANGER.ordinal()] = new RangerExecutor();
 		executors[UnitType.KNIGHT.ordinal()] = new KnightExecutor();
 		executors[UnitType.HEALER.ordinal()] = new HealerExecutor();
 		executors[UnitType.MAGE.ordinal()] = new MageExecutor();
 		executors[UnitType.WORKER.ordinal()] = new WorkerExecutor(moveManager);
-
+		
 		while (true) {
 			try {
 				benchmark.push();
 				if (benchmark.peek() / 1000000 < gc.getTimeLeft() - 2000) {
+					benchmark.push();
 					RoundInfo.update();
 					communication.update();
-					if (benchmark.peek() / 1000000 < gc.getTimeLeft() - 3000) {
+					debugPop(benchmark, 5, "Round Update Time: %fms");
+					if ((benchmark.peek() / 1000000 < gc.getTimeLeft() - 3000) || (RoundInfo.getRoundNumber() % 5 == 0)) {
 						try {
+							benchmark.push();
 							moveManager.updateBFS();
+							debugPop(benchmark, 5, "UpdateBFS Time: %fms");
 						} catch (Exception ex) {
-							System.out.println("BFS Exception: "+ex.getMessage());
+							System.out.println("BFS Exception: " + ex.getMessage());
+							ex.printStackTrace();
 						}
 					} else {
 						System.out.println("Skipping BFS Update");
 					}
+					benchmark.push();
 					moveManager.move(unit -> {
 						try {
 							if(executors[unit.getType().ordinal()] != null) {
+								benchmark.push();
 								executors[unit.getType().ordinal()].execute(unit);
+								debugPop(benchmark, 5, "Execution Time: " + unit.getType() + " - %fms");
 							}
 						} catch (Exception ex) {
 							System.out.println("Execution Exception: "+ ex.getMessage());
@@ -58,15 +64,18 @@ public class MarsPlayer {
 				} else {
 					System.out.println("Skipping Round: " + gc.getRoundNumber() + " - " + gc.getTimeLeft() + "ms");
 				}
-				double deltaTime = benchmark.pop() / 1000000.0;
-				if(deltaTime > 20) {
-					System.out.println("Round Time: " + gc.getRoundNumber() + " - " + deltaTime + "/" + gc.getTimeLeft() + "ms");
-				}
+				debugPop(benchmark, 20, "Round Time: " + gc.getRoundNumber() + " - %f/" + gc.getTimeLeft()+"ms");
 			} catch (Exception ex) {
-				System.out.println("Mystery Exception: "+ex.getMessage());
+				System.out.println("Mystery Exception: " + ex.getMessage());
 				ex.printStackTrace();
 			}
 			gc.yield();
+		}
+	}
+	public static void debugPop(Benchmark benchmark, int threshold, String message) {
+		double deltaTime = benchmark.pop() / 1000000.0;
+		if (deltaTime > threshold) {
+			System.out.println(String.format(message, deltaTime));
 		}
 	}
 }
