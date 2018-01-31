3280c27cec5578580a0d8ec969a1b66336eea938
David Shen <me@pantherman594.com>
2018-01-31 00:08:23 -0500

    generate heat map (unused)

--- Bot/src/citricsky/battlecode2018/main/EnemyMap.java
+++ Bot/src/citricsky/battlecode2018/main/EnemyMap.java
@@ -1,15 +1,15 @@
 package citricsky.battlecode2018.main;
 
-import citricsky.battlecode2018.library.GameController;
-import citricsky.battlecode2018.library.MapLocation;
-import citricsky.battlecode2018.library.Unit;
+import citricsky.battlecode2018.library.*;
 import citricsky.battlecode2018.util.Constants;
 
-import java.util.*;
+import java.util.Arrays;
+
 
 public class EnemyMap {
 	private static int[] scoreCache;
 	private static int[] updateTime;
+	private static int[][] heatMap;
 
 	static {
 		scoreCache = new int[Constants.MAX_UNIT_ID];
@@ -33,5 +33,33 @@ public class EnemyMap {
 		int numEnemies = (int) Arrays.stream(nearby).filter(unit -> unit.getTeam() == GameController.INSTANCE.getEnemyTeam()).count();
 		int numFriendlies = nearby.length - numEnemies;
 		return numEnemies - (2 * numFriendlies);
+	}
+
+	public static void updateHeatMap() {
+		Planet planet = GameController.INSTANCE.getPlanet();
+		heatMap = new int[planet.getWidth()][planet.getHeight()];
+		for (Unit unit : GameController.INSTANCE.getAllUnits()) {
+			if (!unit.getLocation().isOnMap() || unit.getType() == UnitType.FACTORY || unit.getType() == UnitType.ROCKET) continue;
+
+			int mult = unit.getTeam() == GameController.INSTANCE.getTeam() ? 1 : -1;
+
+			MapLocation loc = unit.getLocation().getMapLocation();
+			Vector pos = loc.getPosition();
+			int cX = pos.getX();
+			int cY = pos.getY();
+
+			heatMap[cX][cY] += 3*mult;
+
+			for (MapLocation targetLoc : loc.getAllMapLocationsWithin(unit.getVisionRange())) {
+				Vector targetPos = targetLoc.getPosition();
+				heatMap[targetPos.getX()][targetPos.getY()] += 1*mult;
+			}
+			if (unit.getType() == UnitType.RANGER) {
+				for (MapLocation targetLoc : loc.getAllMapLocationsWithin(unit.getRangerCannotAttackRange())) {
+					Vector targetPos = targetLoc.getPosition();
+					heatMap[targetPos.getX()][targetPos.getY()] -= 1*mult;
+				}
+			}
+		}
 	}
 }
